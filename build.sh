11bbbf71aea7af8d45826e76cadc941df71be455
Stephane Alnet <stephane@shimaore.net>
2014-08-23 19:34:09 +0200

    corrected names

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-egrep 'case.*opcode' ../frifri-mcu-code/main.c | sed 's/^.*case \([0-9]\+\):.*"\([^"]\+\)".*$/.inline \2 \1/' > opcodes.t
-egrep 'case.*opcode' ../frifri-mcu-code/main.c | sed 's/^.*case \([0-9]\+\):.*"\([^"]\+\)".*$/\1 :opcode \2/' > opcodes.f
+egrep 'case.*opcode' ../connected-documents-mcu-code/main.c | sed 's/^.*case \([0-9]\+\):.*"\([^"]\+\)".*$/.inline \2 \1/' > opcodes.t
+egrep 'case.*opcode' ../connected-documents-mcu-code/main.c | sed 's/^.*case \([0-9]\+\):.*"\([^"]\+\)".*$/\1 :opcode \2/' > opcodes.f
 (cd fas && make clean && make) || exit 1
 cat types.t opcodes.t bootstrap.t | fas/fas > forth.bin 2>forth.bin.log || exit 1
 
 hexdump -v -e '/1 "0x%02x, "' -e '/1 " /* %06_ax %_u */\n"' forth.bin > forth.bin.h
-cc -o emulate -D__EMULATE -I. ../frifri-mcu-code/main.c || exit 1
+cc -o emulate -D__EMULATE -I. ../connected-documents-mcu-code/main.c || exit 1
 
 FORTH_LIBS="
            opcodes.f boot/rstack.f \
